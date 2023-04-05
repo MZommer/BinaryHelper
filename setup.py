74396a7b1fcdbf79551eae88968a31fa0ee1c500
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-04-05 10:53:49 -0300

    Added module installer

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_packages
+
+setup(name='BinaryHelper',
+      version='1',
+      description="Python Module for Writing and Reading files",
+      author='Matias Zommer',
+      url='https://github.com/MZommer/BinaryHelper',
+      packages=find_packages()
+    )
