c7e7f59b94a9246ec2786fc789fff63a896f26da
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-04-05 11:01:42 -0300

    Update __types__.py

--- BinaryHelper/__types__.py
+++ BinaryHelper/__types__.py
@@ -1,11 +1,10 @@
 from numpy import (
     dtype,
-    _bool,
     int8, uint8,
     int16, uint16,
     int32, uint32,
     int64, uint64,
-    float16, float32, float64, 
+    float16, float32, float64,
     float96, float128, complex64, complex128, complex192, complex256,  # unused
 )
 from .IO import Endianess
@@ -32,21 +31,22 @@ double = float64
 ### Types utils ###
 
 type_format_table = {
-    int8: "b",  uint8: "B",  # byte
-    int16: "h", uint16: "H", # short
-    int32: "i", uint32: "I", # int
-    int64: "q", uint64: "Q", # long long
-    float16: "e", # half
-    float32: "f", # float
-    float64: "d", # double
-    _bool: "?",
+    int8: "b", uint8: "B",  # byte
+    int16: "h", uint16: "H",  # short
+    int32: "i", uint32: "I",  # int
+    int64: "q", uint64: "Q",  # long long
+    float16: "e",  # half
+    float32: "f",  # float
+    float64: "d",  # double
     bool: "?",
     Endianess.BIG: "<", Endianess.LITTLE: ">",
 }
 
+
 def get_type_format(type: dtype, default: str = "x") -> str:
     """dtype to struct format char"""
     return type_format_table.get(type, default)
+
 
 def get_sizeof(type: dtype) -> int:
     return type.itemsize
