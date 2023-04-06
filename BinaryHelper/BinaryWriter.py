af96d88f909d5c640518f1eb892a04fd618e4998
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-04-06 10:35:55 -0300

    Update BinaryWriter.py

--- BinaryHelper/BinaryWriter.py
+++ BinaryHelper/BinaryWriter.py
@@ -2,49 +2,49 @@ from .IO import BaseIO
 from .__types__ import *
 from .logger import logger
 
+
 class BinaryWriter(BaseIO):
 
-    
     def raw(self, value: bytes):
         self.sizeOf += len(value)
         return self.write(value)
-    
+
     def bytes(self, value: bytes):
         return self.raw(value)
-    
+
     def auto(self, value: any):
         # TODO: add support for lists and tuples
         self.sizeOf += get_sizeof(value)
         return self.pack_into(self._endianess_sign + get_type_format(type(value)), value)
-    
+
     ### Types ###
-    
+
     def bool(self, value: bool):
         if not isinstance(value, bool):
             value = bool(value)
         return self.auto(value)
-    
+
     def int8(self, value: int8):
         if not isinstance(value, int8):
             logger.warning(f"Casting {type(value).__name__} into an int8, risk of a data loss")
             value = int8(value)
         return self.auto(value)
-    
+
     def uint8(self, value: uint8):
         if not isinstance(value, uint8):
             logger.warning(f"Casting {type(value).__name__} into an uint8, risk of a data loss")
             value = uint8(value)
         return self.auto(value)
-    
+
     def char(self, value: int8):
         return self.int8(value)
-    
+
     def uchar(self, value: uint8):
         return self.uint8(value)
-    
+
     def byte(self, value: int8):
         return self.int8(value)
-    
+
     def ubyte(self, value: uint8):
         return self.uint8(value)
 
@@ -59,10 +59,10 @@ class BinaryWriter(BaseIO):
             logger.warning(f"Casting {type(value).__name__} into an uint16, risk of a data loss")
             value = uint16(value)
         return self.auto(value)
-    
+
     def short(self, value: int16):
         return self.int16(value)
-    
+
     def ushort(self, value: uint16):
         return self.uint16(value)
 
@@ -80,13 +80,13 @@ class BinaryWriter(BaseIO):
 
     def int(self, value: int32):
         return self.int32(value)
-    
+
     def uint(self, value: uint32):
         return self.uint32(value)
-    
+
     def long(self, value: int32):
         return self.int32(value)
-    
+
     def ulong(self, value: uint32):
         return self.uint32(value)
 
@@ -101,10 +101,10 @@ class BinaryWriter(BaseIO):
             logger.warning(f"Casting {type(value).__name__} into an uint64, risk of a data loss")
             value = uint64(value)
         return self.auto(value)
-    
+
     def longlong(self, value: int64):
         return self.int64(value)
-    
+
     def ulonglong(self, value: uint64):
         return self.uint64(value)
 
@@ -113,23 +113,23 @@ class BinaryWriter(BaseIO):
             logger.warning(f"Casting {type(value).__name__} into a float16, risk of a data loss")
             value = float16(value)
         return self.auto(value)
-    
+
     def half(self, value: float16):
         return self.float16(value)
-    
+
     def float32(self, value: float32):
         if not isinstance(value, float32):
             if not isinstance(value, float):
                 logger.warning(f"Casting {type(value).__name__} into a float32, risk of a data loss")
             value = float32(value)
         return self.auto(value)
-    
+
     def float(self, value: float32):
         return self.float32(value)
-    
+
     def single(self, value: float32):
         return self.float32(value)
-    
+
     def float64(self, value: float64):
         if not isinstance(value, float64):
             if not isinstance(value, float):
